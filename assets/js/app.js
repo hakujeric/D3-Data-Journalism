53f7993facac907bc885fed8672aacb3a491403c
Eric H <haku_jeric@yahoo.ca>
2019-05-25 13:03:44 -0400

    added svg configuration

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,38 @@
-// @TODO: YOUR CODE HERE!
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 80,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart,
+// and shift the latter by left and top margins.
+var svg = d3
+  .select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+// Append an SVG group
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Initial Params
+var chosenXAxis = "in_poverty";
+var chosenYAxis = "obese";
+
+
+// Load data from data.csv
+d3.csv("assets/data/data.csv", function(error, data) {
+  // Log an error if one exists
+  if (error) return console.warn(error);
+  // Print the Data
+  console.log(data);
+
+});
